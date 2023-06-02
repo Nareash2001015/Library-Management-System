27abbbbeb05e0e2436ea731fa1852f005326c3f5
Mr.Nareash <nareash20010150@gmail.com>
2023-06-02 18:05:32 +0530

    Gave different priviledges to different uses with the login form

---
+++ src/main/java/com/example/LibraryManagementSystem/config/UserRole.java
@@ -0,0 +1,19 @@
+package com.example.LibraryManagementSystem.config;
+
+public enum UserRole {
+
+        /*
+        All CRUD Operations
+        */
+        ADMIN,
+
+        /*
+        CRU Operations
+        */
+        PUBLISHER,
+
+        /*
+        Read_only Operations
+        */
+        READ_ONLY
+}
